85f0cba0b040eeca5cdaec95f7b6db3efba7f2ed
Eya132 <99542434+Eya132@users.noreply.github.com>
2022-03-08 14:00:41 +0100

    Update minimap.h

--- minimap.h
+++ minimap.h
@@ -1,3 +1,17 @@
-void initminimap( minimap * m);
-void MAJMinimap(perso p , minimap * m, SDL_Rect camera, int redimensionnement);
-void afficherminimap (minimap m, SDL_Surface * screen);
+struct minimap
+{
+	SDL_Surface* mini_map;
+	SDL_Rect pos_minimap;
+	SDL_Surface* mini_hero;
+	SDL_Rect pos_minihero;
+	
+	
+};
+typedef struct minimap minimap; 
+
+
+void init_minimap(minimap* mp);
+void blit_minimap( minimap* mp,SDL_Surface *screen);
+void dep_mini_hero (SDL_Rect* pos_perso,minimap mp , SDL_Rect camera,int redimensionnement,int sens);
+void free_minimap(minimap* mp);
+
